b93586002ef13dcb1304da0955cc42dcb73d224f
1F10240199 <s1f102401999@iniad.org>
2025-07-09 17:02:18 +0900

    requirements.txt 追加4

--- todo/views.py
+++ todo/views.py
@@ -17,7 +17,7 @@ def index(request):
     else:
         tasks = Task.objects.order_by('-posted_at')
 
-    tasks = Task.objects.all()
+    # tasks = Task.objects.all()
 
     context = {
         'tasks': tasks
